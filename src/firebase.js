9c2b22069377c616b8047b0bf74268c77018e3ca
SaaS Builder <builder@saas.app>
2026-05-14 17:29:50 +0000

    Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfdCgFzkrZ_YvFnzbBgRG9nyPLq8qLgBY",
-  authDomain: "bright-kids-school-sytem.firebaseapp.com",
-  projectId: "bright-kids-school-sytem",
-  storageBucket: "bright-kids-school-sytem.firebasestorage.app",
-  messagingSenderId: "296531828842",
-  appId: "1:296531828842:web:dab68d265001a29f156a13"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
