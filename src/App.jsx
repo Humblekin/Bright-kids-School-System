30fa3a00640883db95b0f5a2a27ba31c3c309c4e
SaaS Builder <builder@saas.app>
2026-05-15 07:52:26 +0000

    Revert "Remove parent contact and SMS sender"
    This reverts commit d6fd2f36813b16766a1760b79e1d56564b5b9cf9.

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ManageClasses from './pages/admin/ManageClasses';
 import ManageSubjects from './pages/admin/ManageSubjects';
 import ManageFees from './pages/admin/ManageFees';
 import SchoolSettings from './pages/admin/SchoolSettings';
+import SmsSender from './pages/admin/SmsSender';
 import ReportCards from './pages/admin/ReportCards';
 
 import TeacherDashboard from './pages/teacher/TeacherDashboard';
@@ -37,6 +38,7 @@ export default function App() {
           <Route path="/admin/announcements" element={<ProtectedRoute allowedRoles={['admin']}><Announcements /></ProtectedRoute>} />
           <Route path="/admin/reports" element={<ProtectedRoute allowedRoles={['admin']}><ReportCards /></ProtectedRoute>} />
           <Route path="/admin/fees" element={<ProtectedRoute allowedRoles={['admin']}><ManageFees /></ProtectedRoute>} />
+          <Route path="/admin/sms" element={<ProtectedRoute allowedRoles={['admin']}><SmsSender /></ProtectedRoute>} />
           <Route path="/admin/settings" element={<ProtectedRoute allowedRoles={['admin']}><SchoolSettings /></ProtectedRoute>} />
           <Route path="/admin/profile" element={<ProtectedRoute allowedRoles={['admin']}><Profile /></ProtectedRoute>} />
 
